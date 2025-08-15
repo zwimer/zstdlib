562f1376f102f2a34ae1989e59e82da5c0bb62b4
zwimer <github@zwimer.com>
2025-08-15 01:00:45 -0400

    Singleton rewrite

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -1,125 +1,138 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,unused-variable
+from collections.abc import Callable
 from threading import Thread, Lock
+from datetime import datetime
 from time import sleep
 import unittest
 
-from zstdlib.singleton import SingletonType, Singleton
-
-
-class TestSingletonType(unittest.TestCase):
-
-    def test_valid(self) -> None:
-        class ST1(metaclass=SingletonType):
-            pass
-
-        self.assertIs(ST1(), ST1())
-
-        class ST2(metaclass=SingletonType):
-            pass
-
-        self.assertIsNot(ST1(), ST2())
-
-    def test_subclass(self) -> None:
-        class ST3(metaclass=SingletonType):
-            pass
-
-        with self.assertRaises(TypeError):
-
-            class ST4(ST3):
-                pass
-
-    def test_multi_thread(self):
-        """
-        Ensure that SingletonType is thread safe and that constructing an object doesn't delay other threads
-        Technically this is more of a heuristic, but it failing is extremely unlikely
-        """
-
-        class ST5(metaclass=SingletonType):
-            def __init__(self):
-                sleep(0.2)
-
-        class ST6(metaclass=SingletonType):
-            def __init__(self):
-                sleep(0.8)
-
-        lock = Lock()
-        events = []
-        results = []
-
-        def t1() -> None:
-            """
-            Construct an ST6 immediately
-            """
-            with lock:
-                pass
-            events.append("START: ST6()")
-            results.append(ST6())
-            events.append("END: ST6()")
-
-        def t2() -> None:
-            """
-            Construct an ST6 after the first ST6 has started construction but before it has finished
-            """
-            with lock:
-                pass
-            sleep(0.2)
-            events.append("START: ST6()")
-            results.append(ST6())
-            events.append("END: ST6()")
-
-        def t3() -> None:
-            """
-            Construct ST5's after both ST6s have started construction, finishing before either end
-            """
-            with lock:
-                pass
-            sleep(0.4)
-            # Loop Enough times that ST6 wil be complete if it ST5 actually constructed each time
-            for i in range(10):
-                events.append("START: ST5()")
-                results.append(ST5())
-                events.append("END: ST5()")
-
-        threads = (Thread(target=t1), Thread(target=t2), Thread(target=t3))
-        with lock:
-            for i in threads:
-                i.start()
-            # Give threads a moment to construct then let them go
-            sleep(0.2)
-        for i in threads:
-            i.join()
-        # Check results
-        wanted = ["START: ST6()"] * 2 + ["START: ST5()", "END: ST5()"] * 10 + ["END: ST6()"] * 2
-        self.assertEqual(events, wanted)
-        # Check constructed objects
-        self.assertEqual(len(results), 2 + 10)
-        for i in range(9):
-            self.assertIs(results[0], results[i + 1])
-        self.assertIsNot(results[0], results[-1])
-        self.assertIs(results[-1], results[-2])
+from zstdlib.singleton import NoInstanceError, Singleton
 
 
 class TestSingleton(unittest.TestCase):
 
-    def test_valid(self):
-        class S1(Singleton):
+    def test_valid(self) -> None:
+        class ST1(Singleton):
             pass
 
-        self.assertIs(S1(), S1())
-
-        class S2(Singleton):
+        class ST2(Singleton):
             pass
 
-        self.assertIsNot(S1(), S2())
+        self.assertIs(ST1(), ST1())
+        self.assertIsNot(ST1(), ST2())
 
-    def test_subclass(self):
-        class S3(Singleton):
+    def test_subclass(self) -> None:
+        class ST1(Singleton):  # Ok to subtype Singleton base class
             pass
+
+        with self.assertRaises(TypeError):  # Not ok to subtype a subtype of Singleton
+
+            class ST2(ST1):
+                pass
+
+        with self.assertRaises(TypeError):  # Not ok to define __init_subclass__ in Singleton
+
+            class ST3(Singleton):
+                def __init_subclass__(cls, **kwargs):
+                    pass
+
+    def test_instance(self):
+        class ST(Singleton):
+            def __init__(self, _):
+                pass
+
+        self.assertTrue(issubclass(NoInstanceError, TypeError))
+        with self.assertRaises(NoInstanceError):
+            _ = ST.instance()
+        obj = ST(0)
+        self.assertIs(ST.instance(), obj)
 
         with self.assertRaises(TypeError):
+            ST.instance(4)
 
-            class S4(S3):
-                pass
+    def test_multi_thread(self):
+        """
+        Ensure that Singleton is thread safe and that constructing an object doesn't delay other threads
+        Technically this is more of a heuristic, but failing is extremely unlikely if this is thread safe
+        Verifies that classes can construct concurrently, thread-safely, and only one instance is created
+        """
+
+        # Shared state for threads
+        start = [datetime.now()]
+        lock = Lock()
+        events = []
+        results = []
+
+        # Helper functions that can help define thread tests
+        timestamp = lambda: (datetime.now() - start[0]).total_seconds()
+
+        def mk_cls(cls: type, name: str) -> None:
+            fmt = lambda x: f"{timestamp():.1f}: {name} {x} {cls.__name__}()"
+            events.append(fmt("->"))
+            results.append(cls())
+            events.append(fmt("<-"))
+
+        def simple(name: str, cls: type, delay: float) -> Callable:
+            def _t() -> None:
+                with lock:  # Ensure all threads can start simultaneously
+                    pass
+                sleep(delay)
+                mk_cls(cls, name)
+
+            return _t
+
+        # Run all functions
+        def run_all(*funcs: Callable) -> None:
+            ts = [Thread(target=i) for i in funcs]
+            events.clear()
+            results.clear()
+            with lock:
+                for i in ts:
+                    i.start()
+                # Give threads a moment to construct then let them go
+                sleep(0.2)
+                start[0] = datetime.now()
+            for i in ts:
+                i.join()
+
+        # Verify concurrent construction of ST1 and ST2
+        class Fast(Singleton):
+            def __init__(self):
+                sleep(0.3)
+
+        class Slow(Singleton):
+            def __init__(self):
+                sleep(0.8)
+
+        run_all(simple("f1", Slow, 0), simple("f2", Fast, 0.2))
+        want = [
+            "0.0: f1 -> Slow()",
+            "0.2: f2 -> Fast()",
+            "0.5: f2 <- Fast()",
+            "0.8: f1 <- Slow()",
+        ]
+        self.assertEqual(want, events)
+        self.assertEqual(len(results), 2)
+        self.assertIs(results[0], Fast())
+        self.assertIs(results[1], Slow())
+        del Slow, Fast  # Cleanup, for safety
+
+        # Verify that multiple threads constructing the same class only construct one object
+        class ST(Singleton):
+            first = True
+
+            def __init__(self):
+                if self.first:
+                    self.first = False
+                    sleep(0.4)
+
+        run_all(simple("f1", ST, 0), simple("f2", ST, 0.2))
+        self.assertEqual(len(events), 4)
+        self.assertEqual(events[:2], ["0.0: f1 -> ST()", "0.2: f2 -> ST()"])
+        # When the first thread's init finishes, the second will stop blocking and finish
+        self.assertEqual(len(results), 2)
+        for i in results:
+            self.assertIs(i, ST())
+        del ST  # Cleanup, for safety
 
 
 if __name__ == "__main__":
--- zstdlib/singleton.py
+++ zstdlib/singleton.py
@@ -1,58 +1,80 @@
-from threading import Condition, RLock
-from typing import Any
+from typing import TypeVar, Self, Any, cast
+from collections import defaultdict
+from threading import RLock
+from functools import cache
 
 
-class SingletonType(type):
+T = TypeVar("T")
+
+
+class _None:
+    """A unique NoneType"""
+
+
+class NoInstanceError(TypeError):
+    """A TypeError raised when trying to get a Singleton instance before constructing it"""
+
+
+def _no_subclass(*_, **__):
+    raise TypeError("Singleton subclasses may not be subclassed")
+
+
+class _SingletonType(type):
     """
     A thread-safe singleton metaclass
+    Singleton types may not be subclassed except for the base Singleton type
     """
 
-    _seen: set[type] = set()  # Types that should not be constructed again
+    _disallow_init_subclass = False  # If True, disallow __init_subclass__ in __new__'s attrs
     _instances: dict[type, Any] = {}  # Fully constructed and initialized singleton types
-    _lock = Condition()
-    # For preventing subclassing of Singletons
-    _types: list[type] = []
-    _types_lock = RLock()
+    # Lock is preferred to RLock, but could deadlock if user write a constructor that invokes itself
+    _cls_locks: dict[type, RLock] = defaultdict(RLock)
+    _lock = RLock()
 
     def __new__(mcs, name, bases, attrs, **kwargs):
-        """
-        Intercept all class definitions to prevent subclassing singleton types except for Singleton
-        """
-        if "__init_subclass__" in attrs:
-            raise TypeError("Singleton's should not be subclassed or implement __init_subclass__")
-
-        def _init_subclass(cls):
-            with mcs._types_lock:
-                if any(issubclass(cls, i) for i in mcs._types if i is not Singleton):
-                    err = "Do not derive from Singletons other than the base Singleton type"
-                    raise TypeError(err)
-                mcs._types.append(cls)
-
-        attrs["__init_subclass__"] = _init_subclass
+        """Define a new Singleton type"""
         ret = super().__new__(mcs, name, bases, attrs, **kwargs)
-        with mcs._types_lock:
-            mcs._types.append(ret)
+        if mcs._disallow_init_subclass and "__init_subclass__" in attrs:
+            raise TypeError("Singleton subclasses may not define __init_subclass__")
+        with mcs._lock:
+            mcs._cls_locks[ret] = RLock()
         return ret
 
     def __call__(cls, *args, **kwargs):
-        """
-        Intercept all instantiations to ensure at most one instance exists
-        """
-        # If the object has been seen before, wait for it to be available then return it
+        """Intercept all instantiations to ensure at most one instance exists"""
         with cls._lock:
-            if cls in cls._seen:
-                cls._lock.wait_for(lambda: cls in cls._instances)
-                return cls._instances[cls]
-            cls._seen.add(cls)
-        # The object has not been constructed before, create it outside any lock to avoid delays
-        obj = super().__call__(*args, **kwargs)
-        with cls._lock:
-            cls._instances[cls] = obj
-            cls._lock.notify_all()  # Notify other threads of the new object
-            return obj
+            cls_lock = cls._cls_locks[cls]
+        with cls_lock:
+            if (ret := cls._instances.get(cls, _None)) is _None:
+                cls._instances[cls] = (ret := super().__call__(*args, **kwargs))
+        return ret
+
+    @classmethod
+    @cache
+    def instance(mcs, t: type[T]) -> T:
+        """Get the instance of cls"""
+        if not isinstance(t, mcs):
+            raise TypeError(f"{t} is not a Singleton type")
+        with mcs._lock:
+            cls_lock = mcs._cls_locks[t]
+        with cls_lock:
+            if (got := mcs._instances.get(t, _None)) is _None:
+                raise NoInstanceError(f"No instance of {t} found")
+        return got
 
 
-class Singleton(metaclass=SingletonType):
-    """
-    A thread-safe singleton base class
-    """
+class Singleton(metaclass=_SingletonType):
+    """A thread-safe singleton base class"""
+
+    @classmethod
+    def instance(cls) -> Self:
+        """Blocking get the instance of cls; raise if not constructed"""
+        return cast(_SingletonType, type(cls)).instance(cls)
+
+    def __init_subclass__(cls, *args, **kwargs):
+        """Disallow subclassing"""
+        cls.__init_subclass__ = _no_subclass
+
+
+# Disallow __init_subclass__ in Singleton subclasses
+_SingletonType._disallow_init_subclass = True  # pylint: disable=protected-access
