0ae8d0fb95ea2a1b24d6e73007c3fa0aa0231db9
zwimer <github@zwimer.com>
2025-08-14 21:28:37 -0400

    Reduce chance of name collision by subclasses in Frozen

--- zstdlib/frozen.py
+++ zstdlib/frozen.py
@@ -14,28 +14,28 @@ _FN_ATTRS = (
 
 
 class Freezable:
-    """
-    A base class for objects that can be frozen
-    """
+    """A base class for objects that can be frozen"""
 
     def __init__(self, *args, **kwargs) -> None:
+        # Avoid name mangling b/c we use object.__setattr__ etc. on these
+        # Instead use unlikely to collide names for member variables
         super().__init__(*args, **kwargs)  # For MRO super classes
-        if hasattr(self, "_frozen") or hasattr(self, "_can_thaw"):
-            raise ValueError("Base class defines _frozen or _can_thaw")
-        self._can_thaw: bool = True
-        self._frozen: bool = False
+        if hasattr(self, "_freezable_frozen_") or hasattr(self, "_freezable_can_thaw_"):
+            raise ValueError("Base class defines _freezable_frozen_ or _freezable_can_thaw_")
+        self._freezable_can_thaw_: bool = True
+        self._freezable_frozen_: bool = False
 
     # Public
 
     @property
     def frozen(self) -> bool:
         """Check if this object is frozen"""
-        return self._frozen
+        return self._freezable_frozen_
 
     @property
     def thawable(self) -> bool:
         """Check if this object can be thawed or is unfrozen"""
-        return self._can_thaw
+        return self._freezable_can_thaw_
 
     def freeze(self, *, permanent: bool = False):
         """
@@ -43,24 +43,24 @@ class Freezable:
         This function can be called on an already frozen object
         """
         if permanent:
-            object.__setattr__(self, "_can_thaw", False)
-        object.__setattr__(self, "_frozen", True)
+            object.__setattr__(self, "_freezable_can_thaw_", False)
+        object.__setattr__(self, "_freezable_frozen_", True)
 
     def thaw(self):
         """
         Allow modifications to this object
         """
-        if not self._can_thaw:
+        if not self._freezable_can_thaw_:
             raise RuntimeError("Cannot thaw permanently frozen object")
-        object.__setattr__(self, "_frozen", False)
+        object.__setattr__(self, "_freezable_frozen_", False)
 
     def __setattr__(self, key: str, value) -> None:
-        if getattr(self, "_frozen", False):
+        if getattr(self, "_freezable_frozen_", False):
             raise AttributeError("Cannot modify frozen object")
         super().__setattr__(key, value)
 
     def __delattr__(self, item: str) -> None:
-        if getattr(self, "_frozen", False):  # B/c invoked before __init__ defines _frozen
+        if getattr(self, "_freezable_frozen_", False):  # B/c invoked before __init__ defines _frozen
             raise AttributeError("Cannot modify frozen object")
         super().__delattr__(item)
 
@@ -87,16 +87,16 @@ def frozen(arg: str | type):
         def new_method(self, *args, **kwargs):
             ret = original_method(self, *args, **kwargs)
             # pylint: disable=protected-access
-            self._frozen = True  # type: ignore[attr-defined]
+            self._freezable_frozen_ = True  # type: ignore[attr-defined]
             return ret
 
         def __setattr__(self, key: str, value):
-            if getattr(self, "_frozen", False):
+            if getattr(self, "_freezable_frozen_", False):
                 raise AttributeError("Cannot modify frozen object")
             return original_setattr(self, key, value)
 
         def __delattr__(self, item: str):
-            if getattr(self, "_frozen", False):
+            if getattr(self, "_freezable_frozen_", False):
                 raise AttributeError("Cannot modify frozen object")
             return original_delattr(self, item)
 
