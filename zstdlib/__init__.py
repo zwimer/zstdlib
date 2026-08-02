95c0e16be330a53de2c45543b004dba4985da8de
zwimer <github@zwimer.com>
2026-08-01 20:24:04 -0400

    Expicit __all__ exports

--- zstdlib/__init__.py
+++ zstdlib/__init__.py
@@ -1,7 +1,9 @@
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from .not_set import NotSetType, NotSet
 from .frozen import Freezable, frozen
 from .singleton import Singleton
 from .enum import Enum, auto
 from . import log, io
+
+__all__ = ("Enum", "Freezable", "NotSet", "NotSetType", "Singleton", "auto", "frozen", "io", "log")
--- zstdlib/log/__init__.py
+++ zstdlib/log/__init__.py
@@ -1,2 +1,4 @@
 from .cute import CuteFormatter
 from . import trace
+
+__all__ = ("CuteFormatter", "trace")
